a5bc4100698263b19cda32364781d109bf98bb12
Paulo Montarroios <pauloricardomontarroios@hotmail.com>
2017-11-23 23:05:52 -0200

    Evolucao e relacionamento entre componentes e @Input

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import {Frase} from '../shared/frases.model'
+
+export const FRASES : Array<Frase> = [
+    {fraseEng : 'I Like to learn', frasePtBr: 'Eu gosto de aprender'},
+    {fraseEng : 'My name is Paulo',frasePtBr:'Meu nome é Paulo'},
+    {fraseEng : 'I am 22 years old', frasePtBr : 'Eu tenho 22 anos'},
+    {fraseEng : 'I ate bread', frasePtBr : 'Eu comi pão'}
+];
--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Frase } from '../shared/frases.model'
+import { FRASES } from './frases-mock'
+import { ProgressoComponent } from '../progresso/progresso.component'
 
 @Component({
   selector: 'app-painel',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  public frases : Array<Frase> =  FRASES;
+  public instrucao : string = "Traduza a frase: ";
+  public resposta : string = "";
+  public rodada : number = 0;
+  public rodadaFrase : Frase;
+  public progresso : number = 0;
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada];
+    console.log(this.rodadaFrase);
+   }
 
   ngOnInit() {
   }
 
+  public atualizaResposta(resposta : Event) : void {
+    this.resposta =  (<HTMLInputElement>resposta.target).value;
+    console.log(this.resposta);
+  }
+
+  public validaResposta() : void {
+    if(this.rodadaFrase.frasePtBr == this.resposta){
+        this.rodada++;
+        this.rodadaFrase = this.frases[this.rodada];
+        this.progresso = this.progresso + (100 / this.frases.length)
+        console.log(this.progresso)
+       alert("Acertou !!");
+    }else{
+      this.rodada = 0;
+      this.progresso = 0;
+      this.rodadaFrase = this.frases[this.rodada];
+      alert("Eita, tenta de novo.");
+    }
+  }
 }
